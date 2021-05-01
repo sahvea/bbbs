b3d33c7c10c5af44a98104bb6563d316a22b8109
Sophie <sophie.milash@yandex.ru>
2021-05-01 08:52:51 +0200

    fix: исправить компиляцию scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@
 
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
-const sourcemap = require('gulp-sourcemaps');
+//const sourcemap = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const csso = require('gulp-csso');
 const imagemin = require('gulp-imagemin');
-const svgstore = require('gulp-svgstore');
+//const svgstore = require('gulp-svgstore');
 const rename = require('gulp-rename');
 const posthtml = require('gulp-posthtml');
 const include = require('posthtml-include');
@@ -16,16 +16,16 @@ const del = require('del');
 const server = require('browser-sync').create();
 
 gulp.task('css', function () {
-  return gulp.src('source/sass/style.scss')
+  return gulp.src('source/sass/**/*.scss')
     .pipe(plumber())
-    .pipe(sourcemap.init())
+    //.pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
     .pipe(csso())
-    .pipe(rename('style.css'))
-    .pipe(sourcemap.write('.'))
+    .pipe(rename({prefix: "", suffix: ""}))
+    //.pipe(sourcemap.write('.'))
     .pipe(gulp.dest('build/css'))
     .pipe(server.stream());
 });
@@ -52,6 +52,7 @@ gulp.task('html', function () {
 gulp.task('copy', function () {
   return gulp.src([
     'source/vendor/fonts/**/*.{woff,woff2,eot,ttf,css}',
+    'source/vendor/*.css',
     'source/images/**',
     'source/*.ico'
   ], {

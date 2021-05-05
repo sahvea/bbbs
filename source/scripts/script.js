9f453de80c89d54a6dc4365686ddbe3a9e0a2310
Sophie Milash <sophie.milash@yandex.ru>
2021-05-05 17:00:44 +0200

    feat: добавить разметку для страницы с вопросами

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+/* const tagButtonList = document.querySelectorAll('.tags__button');
+
+function toggleTagButton() {
+  tagButtonList.classList.toggle('tags__button_active');
+}
+
+tagButtonList.forEach(item => {
+  item.addEventListener(`click`, toggleTagButton);
+});
+
+class TagList {
+  constructor (tagButton) {
+    this._tagButton = tagButton;
+  }
+
+  _handleClickButton() {
+    this._tagButton.classList.toggle('tags__button_active');
+  }
+
+  _setEventListeners() {
+    this._tagButton.addEventListener('click', () => this._handleClickButton());
+  }
+}
+
+const  */

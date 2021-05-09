683d9c4a0dee6bf681bbdc3bc9740e802965d339
Sophie <sophie.milash@yandex.ru>
2021-05-09 21:10:13 +0200

    feat: добавить поиск

--- source/scripts/menu.js
+++ source/scripts/menu.js
@@ -1,21 +1,26 @@
-//const listMenu = document.querySelector('.menu__list');
 const header = document.querySelector('.header');
-const listMenuLink = document.querySelectorAll('.menu__link');
+const menu = document.querySelector('.menu');
 const menuBurger = document.querySelector('.menu__burger');
 const menuListSWrap = document.querySelector('.menu__lists-wrap');
 const menuListSocial = document.querySelector('.menu__list_type_social');
+const searchButton = document.querySelector('.search__button');
+const searchOptions = document.querySelector('.search__options');
 
 function toggleMenuBurger() {
   header.classList.toggle('heder_displayed');
   menuBurger.classList.toggle('menu__burger_active');
   menuListSWrap.classList.toggle('menu__lists-wrap_hidden');
   menuListSocial.classList.toggle('menu__list_hidden');
-  //listMenu.classList.toggle('menu__list_active');
+}
 
+function toggleSearchButton() {
+  menu.classList.toggle('menu_state_search');
+  searchOptions.classList.toggle('search__options_visible');
 }
 
 menuBurger.addEventListener('click', toggleMenuBurger);
+searchButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  toggleSearchButton();
+});
 
-/* listMenuLink.forEach((item) => {
-  item.addEventListener('click', toggleMenuBurger);
-}); */
